dfbcd4ef4d6bce6a6e932970d3b2b6e1ff709600
ayousha2025 <K253069@nu.edu.pk>
2026-03-08 02:12:50 +0500

    Update Task#02.java

--- Task#02.java
+++ Task#02.java
@@ -1,28 +1,37 @@
+//A mobile phone store wants to record phone details when new phones arrive. Each phone
+//has a brand name, model, and price. Write a Java program that creates a Mobile class. Use a
+//no-argument constructor to assign default values and a parameterized constructor to assign
+//actual values. Create objects using both constructors inside the main() method and display
+//phone information.
+
 class Mobile {
-    String brand;
-    String model;
-    double price;
+    String BrandName;
+    String Model;
+    float price;
 
-    Mobile() {
-        brand = "Unknown";
-        model = "Unknown";
-        price = 0.0;
+    //default Constructor
+    Mobile(){
+      BrandName = "Unknown";
+      Model = "Unknown";
+      price = 0f;
+    }
+    //Parameterized Constructor
+    Mobile(String BrandName, String Model, float price){
+        this.BrandName = BrandName;
+        this.Model = Model;
+        this.price =price;
     }
 
-    Mobile(String b, String m, double p) {
-        brand = b;
-        model = m;
-        price = p;
+    void display(){
+        System.out.println("\n=== Phone Information ===");
+        System.out.println("Brand Name: " + BrandName);
+        System.out.println("Model: " + Model);
+        System.out.println("Price: " + price);
     }
 
-    void display() {
-        System.out.println("Brand: " + brand + ", Model: " + model + ", Price: $" + price);
-    }
-
-    public static void main(String[] args) {
+    public static void main(){
         Mobile phone1 = new Mobile();
-        Mobile phone2 = new Mobile("Samsung", "Galaxy S23", 999.99);
-
+        Mobile phone2 = new Mobile("Oppo" , "A6 pro" , 250000f);
         phone1.display();
         phone2.display();
     }
