684df30c97191c9e06500f365ad51771cba973c6
ayousha2025 <K253069@nu.edu.pk>
2026-03-08 02:48:58 +0500

    Update Task#05.java

--- Task#05.java
+++ Task#05.java
@@ -1,30 +1,46 @@
-class Programmer {
-    int id;
+//An online coding platform wants to store information about programmers using the
+//platform. Each programmer has an ID, name, preferred programming language, and number
+//of problems solved. Write a Java program that creates a Programmer class and methods to
+//update solved problems and display programmer details. Create objects in the main()
+//method and test the functionality
+
+class Programmer{
+    int ID;
     String name;
-    String language;
-    int problemsSolved;
+    String program_lang;
+    int solved_problems;
 
-    Programmer(int i, String n, String l, int p) {
-        id = i;
-        name = n;
-        language = l;
-        problemsSolved = p;
+    //default Constructor
+    Programmer(){
+        ID = 0;
+        name = "Unknown";
+        program_lang= "Unknown";
+        solved_problems = 0;
     }
-
-    void solveProblems(int newProblems) {
-        problemsSolved += newProblems;
+    //Parameterized Constructor
+    Programmer( int ID,   String name, String program_lang , int solved_problems){
+        this.ID = ID;
+        this.name = name;
+        this.program_lang= program_lang;
+        this.solved_problems = solved_problems;
     }
-
-    void display() {
-        System.out.println("ID: " + id + ", Name: " + name + ", Language: " + language + ", Problems Solved: " + problemsSolved);
+    void updateSolvedProblems(int newProblems){
+        solved_problems+=newProblems;
+    }
+    void display(){
+        System.out.println("\n=== Programmer Information ===");
+        System.out.println("ID: " + ID);
+        System.out.println("Name: " + name);
+        System.out.println("Programming Language: " + program_lang);
+        System.out.println("No: of solved problems: " + solved_problems);
     }
 
     public static void main(String[] args) {
         Programmer prog1 = new Programmer(1, "Alice", "Java", 10);
         Programmer prog2 = new Programmer(2, "Bob", "Python", 5);
 
-        prog1.solveProblems(3);
-        prog2.solveProblems(7);
+        prog1.updateSolvedProblems(3);
+        prog2.updateSolvedProblems(5);
 
         prog1.display();
         prog2.display();
